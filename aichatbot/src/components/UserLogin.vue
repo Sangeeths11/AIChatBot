ddadf3330f5e2c79f1e7ac5b928b870d6ef14b1a
chandrsangee <sangeeths.chandrakumar@fhgr.stud.ch>
2023-10-10 15:54:53 +0200

    userLogin

---
+++ aichatbot/src/components/UserLogin.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="login-container">
+      <h1>Welcome</h1>
+      <img src="@/assets/logo.png" alt="Profile Image" class="profile-img">
+      <input type="text" placeholder="Username">
+      <input type="password" placeholder="Password">
+      <button>Login</button>
+      <div class="helper-texts">
+        <a href="#">Forgot Username / Password?</a> I want this forgot in a seperate color then username and password
+        <a href="#">Don't have an account? Sign up</a>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'UserLogin'
+  }
+  </script>
+  
+  <style scoped>
+  .login-container {
+    max-width: 400px;
+    margin: 0 auto;
+    text-align: center;
+    padding: 20px;
+  }
+  
+  .profile-img {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    margin: 20px 0;
+  }
+  
+  input[type="text"], input[type="password"], button {
+    display: block;
+    width: 100%;
+    margin: 10px 0;
+    padding: 10px;
+    border-radius: 5px;
+    box-sizing: border-box;
+  }
+  
+  button {
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+  }
+  
+  .helper-texts a {
+    display: block;
+    margin: 10px 0;
+    text-decoration: none;
+    color: #333;
+  }
+  </style>
+  
