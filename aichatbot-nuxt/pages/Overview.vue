2fa73ba1b67c17f25ac61c6fb6c0686fcdbd9802
chandrsangee <sangeeths.chandrakumar@fhgr.stud.ch>
2023-10-18 02:39:08 +0200

    ressource is added

--- aichatbot-nuxt/pages/Overview.vue
+++ aichatbot-nuxt/pages/Overview.vue
@@ -5,19 +5,22 @@
         <!-- 6x6 grid -->
         <div class="grid grid-cols-2 gap-4 p-10">
             <!-- Using a v-for loop to generate 35 cards -->
-            <div v-for="i in 4" :key="i" class="card bg-white p-4 rounded shadow-lg">
+            <div v-for="i in subjects" :key="i" class="card bg-white p-4 rounded shadow-lg">
                 <!-- Sample image (replace with actual image paths) -->
                 <img src="path_to_images/image_placeholder.png" alt="Subject Image" class="w-full h-32 object-cover rounded-md">
                 <h2 class="mt-2 text-center">Subject {{ i }}</h2>
                 <button class="absolute bottom-2 left-2 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded focus:outline-none">
                     Lernen
                 </button>
+                <button class="absolute bottom-2 left-2 ml-24 bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded focus:outline-none">
+                    Ressourcen
+                </button>
             </div>
 
             <!-- The last (36th) card with a plus icon -->
             <div class="card bg-white p-4 rounded shadow-lg flex items-center justify-center">
                 <!-- Plus icon -->
-                <button class="bg-blue-500 hover:bg-blue-600 text-white w-12 h-12 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                <button @click="subjects++" class="bg-blue-500 hover:bg-blue-600 text-white w-12 h-12 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto my-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                     </svg>
@@ -27,10 +30,9 @@
     </div>
 </template>
 
-<script>
-export default {
-
-}
+<script lang="ts" setup>
+    const subjects = ref(5);
+    console.log(subjects);
 </script>
 
 <style scoped>
---
+++ aichatbot-nuxt/pages/ressource.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <h1 class="p-10">Ressourcen</h1>
+        
+        <div class="flex justify-center mb-6 w-">
+            <button class="bg-green-600 text-white px-6 py-2 rounded-tl-lg rounded-bl-lg" @click="activeTab = 'video'">Video</button>
+            <button class="bg-gray-200 text-gray-600 px-6 py-2 rounded-tr-lg rounded-br-lg" @click="activeTab = 'document'">Dokument</button>
+        </div>
+        <div class="flex justify-center mt-6">
+            <select v-if="activeTab === 'video'" v-model="selectedVideo" class="select select-bordered w-80">
+                <option :disabled="true">Wähle ein Video</option>
+                <option v-for="video in videos" :key="video">{{ video }}</option>
+            </select>
+            
+            <select v-if="activeTab === 'document'" v-model="selectedDocument" class="select select-bordered w-80">
+                <option :disabled="true">Wähle ein Dokument</option>
+                <option v-for="document in documents" :key="document">{{ document }}</option>
+            </select>
+        </div>
+        <div class="flex justify-center mt-6">
+            <iframe v-if="activeTab === 'video' && selectedVideo" 
+                width="900" height="600" 
+                :src="getYoutubeLink()" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+            </iframe>
+            
+            <iframe v-if="activeTab === 'document' && selectedDocument" :src="`/${selectedDocument}.pdf`" width="900" height="600"></iframe>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    const activeTab = ref('video');
+
+    // Sample video and document names
+    const videos = ref(['Video1', 'Video2', 'Video3']);
+    const documents = ref(['Dokument1', 'Dokument2', 'Dokument3']);
+
+    const selectedVideo = ref('');
+    const selectedDocument = ref('');
+
+    const getYoutubeLink = () => {
+        return "https://www.youtube.com/embed/dQw4w9WgXcQ";
+    };
+</script>
+
+
+
+<style scoped>
+h1 {
+    font-size: 30px;
+    color: #4caf50;
+}
+</style>
