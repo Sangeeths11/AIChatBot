8e0317ab00bf39637d876fd770feec5c1ca77cde
thmaec <thomas.maechler.cds@stud.fhgr.ch>
2023-11-08 00:00:00 +0100

    update file

--- server/chatbots/generalQuestionAnswering.py
+++ server/chatbots/generalQuestionAnswering.py
@@ -12,7 +12,6 @@ from api.endpoints.subjects.model import getSubjectById, updateSubject
 # Definiere eine Klasse für den Chatbot
 
 
-
 class Chatbot:
     def __init__(self, user_id, subject_id, subject_name):
         self.subject_name = subject_name
@@ -29,12 +28,12 @@ You are only allowed to speak in the defined language.
 {history}
 Human: {human_input}
 Assistant:"""
-        
+
         self.template = self.create_template(self.subject_name)
 
         prompt = PromptTemplate(input_variables=["history", "human_input", "subject"], template=self.template)
         self.chain = LLMChain(
-            llm=OpenAI(temperature=0),
+            llm=OpenAI(temperature=0.8),
             prompt=prompt,
             verbose=False,
             memory=ConversationBufferWindowMemory(k=5),
@@ -43,48 +42,57 @@ Assistant:"""
         self.subject_id = subject_id
 
     def create_template(self, subject):
-        t1 = f"""[Personality]  
-    You are a upbeat, fun and experienced Tutor/Mentor from BrainWaive that helps students to learn, quiz and test about their knowledge of their School subjects.
-    Don't go off topic and stay focused on the subject, if the Students asks you something else, remind him to stop procrastinating and get back to his topic in different manners.
-    Never repeat yourself.
-[Instructions]
-    1. Introduce yourself to the student. Compact your messages so it is easy for the student to follow.
-    2. Ask him in which language he wants to be tutored.
-    3. In a socratic manner, have an interview with the student to determine his best learning and quizzing-options one-by-one for his subject({subject}).
-        2.1: Stop your response to wait for the student.
-        2.5. Once the student has written down their response, write your thoughts on what the student said to you in a separate box by creating a markdown line
-    4. Once interview is finished, thank the student and start the lesson or quiz using the configuration you just established with the student.
-    5. Continue asking questions until the student response with **quit**.
-[Example Responses]
-    ```
-    Hey there, {subject} enthusiast! I'm your BrainWaive tutor, here to make {subject} not just make sense, but also make it fun! Let's turn those {subject} woes into wows!
-    Before we dive into the world of <subject related>, which language would you feel most comfortable learning in?```
-    ```
-    Thoughts: student prefers to jump into solving problems right away.
-    ---
-    Great! Jumping straight into problem-solving can be a very effective way to learn. It's hands-on and can help solidify concepts through practice.
-    Now, let's figure out what type of problems you enjoy tackling. Do you find it more engaging to work on real-world scenarios, or do you prefer more abstract problems?
-    ```
+        t1 = f"""Prompt-Template for Tutor- and Mentor-chatbot:
+# Greeting
+    [Fun and upbeat welcome message with references to {subject}]
+    [Catch the student to learn and offer assistance on {subject}]
+
+# Conversation and Engagement
+    [Provide clear and concise answers to questions about {subject}]
+    [Insert fun facts and humorous anecdotes related to {subject}]
+    [Use engaging, upbeat language tailored to {subject}]
+
+#Quiz Feature
+    [Introduce the quiz on {subject} to the student]
+    [Present a series of questions related to {subject}]
+    [Offer multiple-choice answers, if applicable, for {subject}]
+    [Never reveal the answer in your question]
+    [If the answer is wrong, a hint or the answer with an explanation should be given and continued with the next question]
+    [It should have a certain flow, so when answered correctly or revealing the answer, the next question should be asked in the same output]
+
+# Assessment and Feedback
+    [Evaluate the student's quiz answers on {subject}]
+    [Provide a rating or score based on quiz performance in {subject}]
+    [Give constructive feedback and positive reinforcement on {subject}]
+    [Suggest areas for improvement or further study in {subject}]
+
+# Maintaining Focus
+    [If off-topic, gently guide the conversation back to {subject}]
+    [Encourage further inquiry with probing questions about {subject}]
+
+# Closing
+    [Thank the student for their participation in the {subject} session]
+    [Invite them to return for further learning opportunities in {subject}]
 """
-        t2 = """Follow the instructions above. If the student picks a language, you must change your writing to that language. You can change your language to any language you want.
+        t2 = """Follow the behaviour above and be fun and upbeat at all time. Stay accurate with your answers and don't make asumptions.
+        
 {history}
 Human: {human_input}
 Assistant:"""
         return t1 + t2
-    
 
     def get_response(self, user_input):
         return self.chain.predict(human_input=user_input)
-    
-    
+
     def reset_memory(self):
-    	self.chain.memory.clear()
+        self.chain.memory.clear()
 
 
 # Liste der aktiven Chatbots
 active_chatbots = []
 
-#funktion um history zu löschen.
+
+# funktion um history zu löschen.
 def clearConversationHistoryGeneral(userId, subjectId):
     chatbot = next((bot for bot in active_chatbots if bot.user_id == userId), None)
     chatbot.reset_memory()
@@ -134,7 +142,6 @@ def getConversationHistoryGeneral(userId, subjectId):
         print("No subject found")
         return []
     return subject.get("conversationHistoryGeneralQuestions", []), subject.get("conversationHistoryGeneralAnswers", [])
-
 
 # Beispiel
 # user_id = "txzUE0ZwDjgT9rK5zjUu"
